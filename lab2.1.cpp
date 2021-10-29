c59493a88e88bf442578916ede1747aa5cc503c4
Sofia Olshevskaya <85838235+salixaz@users.noreply.github.com>
2021-10-29 15:15:34 +0300

    code of algorithm

---
+++ lab2.1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, i, j;
+    cout << "Input n: ";
+    cin >> n;
+    double x, s, p;
+    s = 0;
+    cout << "Input x: ";
+    cin >> x;
+    if (x < 0)
+    {
+        if (n <= 1)
+        {
+            cout << "Wrong n";
+        }
+        else
+        {
+            for (i = 0; i <= n; i++)
+            {
+                p = 1.0;
+                for (j = i; j <= n - 1; j++)
+                {
+                    p *= (double)i * i + j;
+                }
+                s += p;
+            }
+            cout << "Result: " << s;
+
+        }
+    }
+    else
+    {
+        if (n <= 3)
+        {
+            cout << "Wrong n"; 
+        }
+        else
+        {
+            for (i = 1; i <= n - 2; i++)
+            {
+                s += i - x;
+            }
+            cout << "Result: " << s;
+        }
+
+
+
+    }
+
+}
