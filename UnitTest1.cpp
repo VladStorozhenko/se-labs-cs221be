4f6f0765d2cc08d7024ae7030ade08f1a4a16860
Artur Holiev <92891111+holiev@users.noreply.github.com>
2021-12-02 17:22:35 +0200

    Test_2
    This code consist of checking the functions. I use structure TEST_CALLS() and TEST_METHODS().

---
+++ UnitTest1.cpp
@@ -0,0 +1,67 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include"C:\Users\Андрей\Desktop\software eng\Lab #5\UNIT_test\UNIT_test\UNIT_test.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(calculate_Tests)
+	{
+	public:
+		TEST_METHOD(calculate_get3and4_6returned)
+		{
+			int n = 3;
+			int x = 4;
+			int expected = 6;
+
+			int actual = calculate(n, x);
+
+			Assert::AreEqual(expected, actual);
+		}
+	public:
+		TEST_METHOD(calculate_get3and_1__3returned)
+		{
+			int n = 3;
+			int x = -1;
+			int expected = -3;
+
+			int actual = calculate(n, x);
+
+			Assert::AreEqual(expected, actual);
+		}
+	};
+	TEST_CLASS(checkValidParams_Tests)
+	{
+	public:
+		TEST_METHOD(checkValidParams_get5_exceptionNotThrown)
+		{
+			int n = 5;
+
+			try
+			{
+				checkValidParams(n);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail();
+			}
+		}
+	public:
+		TEST_METHOD(checkValidParams_get1_exceptionNotThrown)
+		{
+			int n = 1;
+
+			try
+			{
+				checkValidParams(n);
+				Assert::Fail();
+			}
+			catch (...)
+			{
+				Assert::IsTrue(true);
+			}
+		}
+	};
+}
